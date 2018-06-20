394b20d64f8aa16894b7c75ed2c9bc58df2e978c
Kristina <krisiety_95@abv.bg>
2018-06-20 13:54:26 +0300

    Add third items

---
+++ Methods. Debugging and Troubleshooting Code - Lab/Sign of Integer Number/Sign of Integer Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sign_of_Integer_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+
+        static void PrintSign(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"The number {num} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {num} is zero.");
+            }
+        }
+    }
+}
