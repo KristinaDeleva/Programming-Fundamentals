a66245937140afbdc1565154912fdc561f9e361b
Kristina <krisiety_95@abv.bg>
2018-06-20 14:43:06 +0300

    .

---
+++ Exam Preparation II/Ladybugs/Ladybugs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ladybugs
+{
+    class Ladybugs
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] field = new int[size];
+            List<int> ladyBug = Console.ReadLine().Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(i => i >= 0 && i < size).ToList();
+
+            foreach (var index in ladyBug)
+            {
+                field[index] = 1;//ladybug here
+            }
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+                string[] commandParts = line.Split(' ');//1 left 5
+                int currentLadyBug = int.Parse(commandParts[0]);
+                string direction = commandParts[1];
+                int flyLength = int.Parse(commandParts[2]);
+                if (direction == "left")
+                {
+                    flyLength *= -1; 
+                }
+                if (currentLadyBug < 0 ||currentLadyBug >= size)
+                {
+                    continue; // outside of field
+                }
+                if (field[currentLadyBug] == 0)
+                {
+                    continue; // no ladybug here
+                }
+                field[currentLadyBug] = 0;//fly away
+                var nextIndexToLand = currentLadyBug;
+                while (true)
+                {
+                    nextIndexToLand += flyLength;
+                    if (nextIndexToLand<0||nextIndexToLand>=size)
+                    {
+                        break;
+                    }
+                    if (field[nextIndexToLand]==1) // there is a ladybug in this cell
+                    {
+                        continue;
+                    }
+                    field[nextIndexToLand] = 1;
+                    break;//just landed
+                }
+            }
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
---
+++ Exam Preparation II/Roli The Coder/Roli The Coder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Roli_The_Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventIdToNames = new Dictionary<int, string>();
+            var eventParticipant = new Dictionary<int, List<string>>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "Time for Code")
+                {
+                    break;
+                }
+                var commandParts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var eventId = 0;
+                if (!int.TryParse(commandParts[0], out eventId))
+                {
+                    continue;
+                }
+                var eventName = commandParts[1];
+                if (!eventName.StartsWith("#"))
+                {
+                    continue;
+                }
+                eventName = eventName.Trim('#');
+                var invalidPart = false;
+                var participants = new List<string>();
+                
+                for (int i = 2; i < commandParts.Length; i++)
+                {
+                    var participant = commandParts[i];
+                    if (!commandParts[i].StartsWith("@"))
+                    {
+                        invalidPart = true;
+                        break;
+                    }
+                    participants.Add(participant);
+                }
+                if (invalidPart)
+                {
+                    continue;
+                }
+                if (eventIdToNames.ContainsKey(eventId))
+                {
+                    var exciting = eventIdToNames[eventId];
+                    if (exciting==eventName)
+                    {
+                        eventParticipant[eventId].AddRange(participants);
+                    }
+                }
+                else
+                {
+                    eventIdToNames[eventId] = eventName;
+                    eventParticipant[eventId] = new List<string>(participants);
+                }
+            }
+
+            var events = eventParticipant
+                .Select(kvp => new
+                {
+                    Id = kvp.Key,
+                    Name = eventIdToNames[kvp.Key],
+                    Participants = kvp.Value.
+                    Distinct()
+                    .OrderBy(p=>p)
+                    .ToList()
+                }).
+                OrderByDescending(kvp => kvp.Participants.Count)
+                .ThenBy(ev => ev.Name).ToList();
+
+            foreach (var ev in events)
+            {
+                var eventId = ev.Id;
+                var evenName = ev.Name;
+                var participants = ev.Participants;
+                Console.WriteLine($"{evenName} - {participants.Count}");
+                foreach (var part in participants)
+                {
+                    Console.WriteLine(part);
+                }
+            }
+        }
+    }
+}
