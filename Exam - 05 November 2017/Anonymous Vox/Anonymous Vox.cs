f61b4f1d97350f5d3747db3436d8734faf640745
Kristina <krisiety_95@abv.bg>
2018-06-20 14:39:57 +0300

    ..

---
+++ Exam - 05 November 2017/Anonymous Vox/Anonymous Vox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Anonymous_Vox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"([A-Za-z]+)(.*)(\1)";
+
+            string[] placeholders = Console.ReadLine().Split("{}".ToCharArray(),StringSplitOptions.RemoveEmptyEntries);
+
+            var matches = Regex.Matches(text, pattern);
+            int count = 0;
+
+            foreach (Match match in matches)
+            {
+                string decoded = match.Groups[1] + placeholders[count++]
+                    + match.Groups[3];
+                text = text.Replace(match.Value, decoded);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
