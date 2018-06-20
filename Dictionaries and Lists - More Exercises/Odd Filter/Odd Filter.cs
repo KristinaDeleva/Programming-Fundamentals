00fdf8bee018579011c7ef04b37b9a6b6795cf5a
Kristina <krisiety_95@abv.bg>
2018-06-20 14:21:35 +0300

    Add twelve task

---
+++ Dictionaries and Lists - More Exercises/Odd Filter/Odd Filter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odd_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .ToList();
+            var numListAverage = numList.Average();
+            numList = numList.Select(x => x = x > numListAverage ? ++x : --x)
+                .ToList();
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
---
+++ Dictionaries and Lists - More Exercises/Sort Times/Sort Times.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sort_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortTime = Console.ReadLine().Split(' ').OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(", ", sortTime));
+
+        }
+    }
+}
